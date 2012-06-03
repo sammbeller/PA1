a26e7707a86bbb6009393c974bd8bad255daaa43
Sam Beller <sammbeller@gmail.com>
2012-06-03 10:30:37 -0400

    Initial edits for review data and computing of proportions

--- movie_data.rb
+++ movie_data.rb
@@ -2,7 +2,7 @@
 
 #Begin parsing movie data
 
-movie_raw_data = File.new("test.txt", "r")
+movie_raw_data = File.new("u.item", "r")
 
 def parse_movies(movie_file)
 
@@ -16,13 +16,9 @@ def parse_movies(movie_file)
 
 	cur_line = movie_file.gets
 
-
-	=begin
-
-		This while loop checks to see if the file has ended and then splits the current line (cur_line)
-		and extracts all the desired data.
-
-	=end
+	
+	#This while loop checks to see if the file has ended and then splits the current line (cur_line)
+	#and extracts all of the desired data.
 
 	while cur_line
 
@@ -41,7 +37,12 @@ def parse_movies(movie_file)
 		count = 0
 
 		movie_array[i][3] = Array.new
+
+		#This will hold the movie's review data
+		movie_array[i][4] = Array.new(2, Array.new)
 		
+		#This while loop puts each of the "tags" into an array together such that all tags make-up one element
+		#of the movie's array
 		while rem < temp_arr.size
 
 			movie_array[i][3][count] = temp_arr[rem].to_i
@@ -63,6 +64,104 @@ def parse_movies(movie_file)
 end
 
 
+#End parsing movie data
+
+def compute_proportions(user)
+
+	total = 0
+
+	user[0][0].each {|el| total += el}
+
+	i = 0
+
+	while i <= 18
+
+		user[0][1][i] = user[0][0][i] / total
+
+		i += 1
+
+	end
+
+end
+
+
+
+
+
+#Begin parsing review data
+
+
+review_raw_data = File.new("BLABLA", "r")
+
+def parse_reviews
+
+	user_array = Array.new
+
+	temp_arr = Array.new
+
+	cur_line = review_raw_data.gets
+
+	while cur_line
+
+		temp_arr = cur_line.split
+
+		temp_arr.each {|el| el.to_i}
+
+
+		#If the user's array is empty, intialize
+		if !(user_array[temp_arr[0]])
+
+			user_array[temp_arr[0]] = Array.new
+
+			user_array[0][0] = Array.new(2, Array.new)
+
+			user_array[0][1] = Array.new(2, Array.new)
+
+			user_array[0][0][0] = Array.new(18, 0)
+
+			user_array[0][0][1] = Array.new(18, 0)
+
+		end
+
+		tag_arr = movies[temp_arr[1]][3]
+
+		i = 0
+
+		while i <= 18
+
+			user_array[0][0][i] +=  tag_arr[i]
+
+			i += 1
+
+		end
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
 movies = parse_movies(movie_raw_data)
+
+
+
+
+
+
+
+
+
+
 
 
